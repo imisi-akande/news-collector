c8e8e2928ab717dad938ea8817ed24173071ab4d
Imisioluwa Akande <imisioluwa.akande@andela.com>
2020-05-18 21:42:38 +0100

    feat:write-data-to-mongodb

--- news_crawler/news_crawler/settings.py
+++ news_crawler/news_crawler/settings.py
@@ -27,7 +27,8 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,9 +65,11 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'news_crawler.pipelines.NewsCrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'news_crawler.pipelines.MongoDBPipeline': 300,
+    }
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'sahara_news'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
